e00f54872ad5a702e6d7e12ea70c0cf237b67f58
Sanya Hussain <sanz_133@hotmail.com>
2025-04-21 20:53:18 +0100

    enabled cors

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const cors = require("cors");
+
 const { handleCustomErrors, handleServerErrors } = require("./errors");
 const { taskRouter } = require("./BE/taskrouter");
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/tasks", taskRouter);
 
