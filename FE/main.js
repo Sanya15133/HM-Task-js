c2541151293f0fed49b9d1bd505a43e01720ef13
Sanya Hussain <sanz_133@hotmail.com>
2025-04-22 15:35:03 +0100

    used fetch instead

--- FE/main.js
+++ FE/main.js
@@ -1,5 +1,14 @@
-const axios = require("axios");
-
-const taskRouter = axios.create({
-  baseURL: "http://localhost:3000/tasks",
-});
+async function getTasks() {
+  const url = "http://localhost:3000/tasks";
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+    const parseResponse = await response.json();
+    console.log(parseResponse);
+    return parseResponse;
+  } catch (error) {
+    console.error(error.message);
+  }
+}
--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const cors = require("cors");
+console.log("hello from be");
 
 const { handleCustomErrors, handleServerErrors } = require("./errors");
 const { taskRouter } = require("./BE/taskrouter");
